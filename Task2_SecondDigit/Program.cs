39827bb2beefd53e6144722103a1359f2f1f86a7
 Anastasia <anastaasiaan@gmail.com>
2023-09-17 21:39:09 +0300

    Delete Second Digit

---
+++ Task2_SecondDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int DelSecondDig(int number)
+{
+    int firstDig = number / 100;
+    int secondDig = number % 10;
+
+    int result = firstDig * 10 + secondDig;
+    return result;
+}
+
+int randNumd = new Random().Next(100, 1000);
+System.Console.WriteLine($"{randNumd} - > {DelSecondDig(randNumd)}");
