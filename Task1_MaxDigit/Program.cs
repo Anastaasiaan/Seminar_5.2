5c7abd12d6f51f61855a8839f1a0d1a0721fb470
 Anastasia <anastaasiaan@gmail.com>
2023-09-17 21:29:47 +0300

    Find Max Digit

---
+++ Task1_MaxDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.*/
+
+int MaxDigit(int num)
+{
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+if (firstDigit > secondDigit)
+return firstDigit;
+else
+return secondDigit;
+}
+
+int number = new Random().Next(10, 100); // [a, b)
+int res = MaxDigit(number);
+Console.WriteLine($"В числе {number} максимальная цифра {res}");
